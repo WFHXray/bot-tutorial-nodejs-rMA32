4c0909ad84c2e1bb63d34e85bfc465ca887e4533
WFHXray <31389502+WFHXray@users.noreply.github.com>
2017-08-27 13:29:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/com/;
-      botRegexTeam = /^\/i;
+      //botRegexTeam = /^\/i;
       botRegexDepth = /^\/D/i;
       botRegexRules = /^\/rules/;
       botRegexStrikes = /^\/strikes/;
